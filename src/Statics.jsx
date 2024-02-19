0066fdf785de4aa0048a1b53448eaea64cd8e541
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2024-02-19 19:42:47 +0100

    Delete iddle import

--- src/Statics.jsx
+++ src/Statics.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "./Button";
 
 const Statics = ({ good, neutral, bad, anecdotes }) => {
   const [selectedAnecdote, setAnecdote] = useState(
